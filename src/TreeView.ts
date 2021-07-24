64847a16391d406c28c56f58ba9df632d892d431
Michael Farrant <michaelfarrant@outlook.com>
2021-07-24 17:54:02 +0100

    Various bits & pieces for TreeViews & TreeItems

--- src/TreeView.ts
+++ src/TreeView.ts
@@ -1,33 +1,73 @@
-class TreeView<t>{
-    
-    root: TreeViewItem<t>;
+import { Generatable } from './Generatable.js';
 
-    constructor(root: TreeViewItem<t>){
+class TreeView<t> implements Generatable<HTMLUListElement>{
+
+    root: TreeViewItem<t>;
+    selected: TreeViewItem<t>;
+
+    constructor(root: TreeViewItem<t>) {
         this.root = root;
+        this.root.addEventListener('click', (e) => {
+            console.log('hello!');
+
+        });
+    }
+
+    getSelected() {}
+
+    generateElement(): HTMLUListElement {
+        const ul = document.createElement('ul');
+
+        this.root.generateElement();
+
+        this.root.getChildren().forEach(child=>ul.append(child.generateElement()));
+
+        return ul;
     }
 
 }
 
-class TreeViewItem<t>{
+class TreeViewItem<t> extends EventTarget implements Generatable<HTMLLIElement>{
 
     parent: TreeViewItem<t>;
     children: Array<TreeViewItem<t>>;
-    value: t; 
+    value: t;
 
-    constructor(value: t){
+    constructor(value: t) {
+        super();
+
         this.children = new Array();
-        this.parent   = null;
-        this.value    = value;
+        this.parent = null;
+        this.value = value;
     }
 
-    addChild(child: TreeViewItem<t>): void{
+    getChildren(): Array<TreeViewItem<t>>{
+        return this.children;
+    }
+
+    generateElement(): HTMLLIElement {
+        const li = document.createElement('li');
+
+        li.append(document.createTextNode('Test'));
+
+        return li;
+    }
+
+    getValue(): t {
+        return this.value;
+    }
+
+    addChild(child: TreeViewItem<t>): void {
         this.children.push(child);
-
     }
 
-    setParent(parent: TreeViewItem<t>){
+    setParent(parent: TreeViewItem<t>) {
         this.parent = parent;
+    }
+
+    _emitClick(): void {
+        this.dispatchEvent(new CustomEvent('click', { bubbles: true }));
     }
 }
 
-export{TreeView,TreeViewItem};
+export { TreeView, TreeViewItem };
