3ee9440a0444bd616aa8ef42e039644047d62701
Michael Farrant <michaelfarrant@outlook.com>
2021-08-24 23:08:53 +0100

    Made the tree view more compact by remving the padding in the master & sub ul's

--- src/CssClasses.ts
+++ src/CssClasses.ts
@@ -4,7 +4,7 @@ export const TREE_VIEW_ITEM = 'tree-view-item';
 
 export const TREE_SELECTED = 'tree-selected';
 
-export const TREE_VIEW_UL = 'tree-view-ul';
+export const TREE_VIEW_UL = 'tree-view';
 
 export const TREE_VIEW_ITEM_EXPANDED = 'tree-view-item-expanded';
 
