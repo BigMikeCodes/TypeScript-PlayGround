6d3a8eb5f5f6f718febb493888363b20469a6ec3
Michael Farrant <michaelfarrant@outlook.com>
2021-11-08 17:30:56 +0000

    Renmaed Content to ContentApi

--- src/api/ContentApi.ts
+++ src/api/ContentApi.ts
@@ -1,17 +1,15 @@
-export class Content{
+export class ContentApi{
 
     private static async getContent(path: string): Promise<Response>{
         const url = `http://127.0.0.1:8006/api/content/get${path}`; 
         return fetch(url,{method: 'GET'});
     }
 
+    static async getPlainText(path: string): Promise<string>{
 
-    static async clear(path: string): Promise<string>{
-
-        const response = await Content.getContent(path);
+        const response = await ContentApi.getContent(path);
         return await response.text();
 
     }
-
 
 }
